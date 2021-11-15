05f3bdfebe24c8e9a04b99ea47319e243b1bdb3f
Oliver Kogel <kogel.oliver@web.de>
2021-11-15 21:32:56 +0100

    socket-01-unix.c Kommentierung (#89)
    * socket comments
    
    * Add Disclaimer comment
    
    Co-authored-by: Jonathan <26322754+nukerxy@users.noreply.github.com>

--- RES-GUENTHER/extra/socket/socket-01-unix.c
+++ RES-GUENTHER/extra/socket/socket-01-unix.c
@@ -1,3 +1,7 @@
+/* Dieses File ist keine valide Lösung für socket-01-unix.c
+Es ist als Exkurs in die AF_UNIX Sockets zur IPC zu verstehen.
+siehe PR #87, lange Diskussion, weitere Infos
+*/
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,40 +19,50 @@ int main(int argc, char *argv[])
 {
     printf("start \n");
     fflush(stdout);
+    // if argument 1 == "server", we are the server
     bool isServer=false;
     if  (argc>1 && strcmp(argv[1],"server")==0){
         isServer=true;
     }
+    // this struct manages our socket
     struct sockaddr_un addr;
+    // the buffer we write data to
     char message[2000];
     int struct_length = sizeof(addr);
 
+    // clear message buffer
     memset(message, '\0', sizeof(message));
 
+    // socket ID that is generated, when socket is created
     int socketint;
     printf("init \n");
     fflush(stdout);
 
+    // create a dgram socket in the unix domain
     socketint = socket(AF_UNIX, SOCK_DGRAM,0);
     if(socketint < 0){
         printf("Error while creating socket\n");
         fprintf(stderr, "error %s", strerror(errno));
         return -1;
     }
+    // define socket type
     addr.sun_family = AF_UNIX;
-  //  unlink(SOCKET_NAME);
+
+    // set socket filename/path
     strncpy(addr.sun_path, SOCKET_NAME, sizeof(addr.sun_path) - 1);
     printf("So \n");
     fflush(stdout);
-    //server
+    // server
     if (isServer){
         printf("Server \n");
 
+        // bind the socket
         if(bind(socketint, (struct sockaddr*)&addr, sizeof(addr)) < 0){
             printf("Couldn't bind to the port\n");
             return -1;
         }
         int struct_length = sizeof(addr);
+        // wait for a message to arrive and write it to the message buffer
         if (recvfrom(socketint, (char *)message, sizeof(message), 0,(struct sockaddr*)&addr, &struct_length) < 0){
             printf("Couldn't receive\n");
             return -1;
@@ -56,11 +70,13 @@ int main(int argc, char *argv[])
         printf("Msg from client: %s\n", message);
         close(socketint);
     }
-    //Client
+    // client
     else{
         sprintf(message,"Hallo Server");
         printf("%s \n",message);
         fflush(stdout);
+
+        // write the contents of the message buffer to the socket, effectively sending it
         if(sendto(socketint, (const char *)message, strlen(message), 0,(const struct sockaddr*)&addr, struct_length) < 0){
             printf("Unable to send message\n");
             fprintf(stderr, "error %d", errno);
