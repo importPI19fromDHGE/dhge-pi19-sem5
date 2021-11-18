52d490bff4d590e25198a491aefe8b60edaf1ee5
Oliver Kogel <kogel.oliver@web.de>
2021-11-18 20:20:18 +0100

    Update main.rs

--- RES-GUENTHER/extra/socket/chat-rust/src/main.rs
+++ RES-GUENTHER/extra/socket/chat-rust/src/main.rs
@@ -16,7 +16,7 @@ use std::thread;
 const SERVER_ADDR: &str = "127.0.0.1:6000";
 const CLIENT_ADDR: &str = "127.0.0.1:6001";
 // hardcode a buffer size of 4096 bytes
-// if a sent messages exceeds this size, the program will panic!
+// if a sent message exceeds this size, the program will panic!
 const BUFFER_SIZE: usize = 4096;
 
 fn main() {
@@ -26,57 +26,51 @@ fn main() {
     // parse command line arguments
     // if first argument == "server", we run in server mode
     if args.len() > 1 && args[1] == "server" {
-        server().unwrap();
+        server();
     } else {
-        client().unwrap();
+        client();
     }
 }
 
 // initialize the server side
-fn server() -> std::io::Result<()> {
-    {
-        println!("Hello, I am the Server.");
+fn server() {
+    println!("Hello, I am the Server.");
 
-        // bind the socket!
-        let socket = UdpSocket::bind(SERVER_ADDR)?;
-        // initalize an empty byte buffer
-        let mut buffer = [0; BUFFER_SIZE];
+    // bind the socket!
+    let socket = UdpSocket::bind(SERVER_ADDR).unwrap();
+    // initalize an empty byte buffer
+    let mut buffer = [0; BUFFER_SIZE];
 
-        // wait for a message from the client, read it into the buffer
-        // amt is the amount of bytes received
-        // src is the sender's address
-        let (amt, src) = socket.recv_from(&mut buffer)?;
+    // wait for a message from the client, read it into the buffer
+    // amt is the amount of bytes received
+    // src is the sender's address
+    let (amt, src) = socket.recv_from(&mut buffer).unwrap();
 
-        println!("Server: {} bytes read from source {}!", amt, src);
-        println!(
-            "Server: Message received from client: {}",
-            str::from_utf8(&buffer).unwrap()
-        );
+    println!("Server: {} bytes read from source {}!", amt, src);
+    println!(
+        "Server: Message received from client: {}",
+        str::from_utf8(&buffer).unwrap()
+    );
 
-        // now that we know who the client is, we can enter the messaging loop
-        messaging_loop(src, socket).unwrap();
-    }
-    Ok(())
+    // now that we know who the client is, we can enter the messaging loop
+    messaging_loop(src, socket).unwrap();
 }
 
 // initialize the client side
-fn client() -> std::io::Result<()> {
-    {
-        println!("Hello, I am the Client.");
+fn client() {
+    println!("Hello, I am the Client.");
 
-        // bind the socket!
-        let socket = UdpSocket::bind(CLIENT_ADDR)?;
-        let buffer = "Hello Server!".as_bytes();
+    // bind the socket!
+    let socket = UdpSocket::bind(CLIENT_ADDR).unwrap();
+    let buffer = "Hello Server!".as_bytes();
 
-        // send an initial message to the server, so it knows who we are
-        socket
-            .send_to(&buffer, SERVER_ADDR)
-            .expect("Could not send message!");
+    // send an initial message to the server, so it knows who we are
+    socket
+        .send_to(&buffer, SERVER_ADDR)
+        .expect("Could not send message!");
 
-        // enter the messaging loop
-        messaging_loop(SERVER_ADDR.parse().unwrap(), socket).unwrap();
-    }
-    Ok(())
+    // enter the messaging loop
+    messaging_loop(SERVER_ADDR.parse().unwrap(), socket).unwrap();
 }
 
 // once client/server is set up and ready, we can start chatting in here
